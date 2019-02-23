56d67899570432a6a27a1681131fc1f1d27afced
gigi <gianluigilamera@yahoo.com>
2019-02-23 22:41:07 +0000

    Updated confirmation screen

--- src/components/confirm.jsx
+++ src/components/confirm.jsx
@@ -11,60 +11,77 @@ export default class Confirm extends React.Component {
         this.state = {
             validation_message: ""
         }
+
+        //Holds timing offset for checking the email validation
         this.timer = null;
-        this.input = React.createRef();
-        this.validation_message = "";
+        //Reference to the email input
+        this.email = React.createRef();
+        this.message = React.createRef();
 
         this.check_email = this.check_email.bind(this);
     }
 
     render(){
-        const message = this.state.validation_message ? <p className="hint">{this.state.validation_message}</p> : "";
+        //If there's no message don't display the element
+        const message = this.state.validation_message ? <p className="hint" ref={this.message}>{this.state.validation_message}</p> : "";
 
         return (
             <>
                 <Headings title="Almost there!" text="Insert your email to be notified when your target is met"/>
+
                 <form action="/">
-                    <label htmlFor="email">
+                    <label htmlFor="email" className="email_label">
                         <p>
                             Your email:
                         </p>
-                        <input type="email" name="email" id="email" required ref={this.input} onInput={this.check_email}/>
+                        <input type="email" name="email" id="email" placeholder="Insert your email" required ref={this.email} onInput={this.check_email}/>
                     </label>
                     {message}
-                    <label htmlFor="terms">
-                        <p>
-                            I read and understand the terms.
-                        </p>
-                        <input type="checkbox" name="terms" id="terms" onChange={this.props.change_handler}/>
+
+                    <label htmlFor="terms" className="terms_label flex_r_nowrap align_center">
+                        <span className="custom_checkbox_container">
+                            <input type="checkbox" name="terms" id="terms" onChange={this.props.change_handler}/>
+                            <span className="custom_checkbox">
+                                <img src={require("../images/interface_icons/tick.svg")} alt="checkbox_tick"/>
+                                
+                            </span>
+                        </span>
+                        I read and understand the terms.
                     </label>
+
                    <ProgressButton>CONFIRM</ProgressButton>
                 </form>
             </>
         )
     }
-
+    //Sets appropriate validation messages into the state, runs at every keystroke (but throttles them)
     check_email({target: {value}}) {
+        //If it's not the first keystroke clear timer
         if(this.timer) {
             clearTimeout(this.timer);
         } 
+
         const trimmed_value = value.trim();
-            this.timer = setTimeout(()=>{
-                if(!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(trimmed_value)) {
-                    this.validation_message = "Email is not valid";
-                } else if (this.props.terms_accepted) {
-                    this.validation_message = "Ready, set, Go!";
-                    this.setState({
-                        validation_message: "Email is valid"
-                    })
-                    this.props.set_email(value);
-                } else {
-                    this.props.set_email(value);
-                    this.setState({
-                        validation_message: "Email is valid"
-                    })
-                }
-            },300)
+        //Set a reference to the timer
+        this.timer = setTimeout(()=>{
+            //Set appropriate messages based on the outcome
+            if(!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(trimmed_value)) {
+                this.setState({
+                    validation_message: "Email is not valid"
+                })
+            //If it's valid and terms have been accepted
+            } else if (this.props.terms_accepted) {
+                this.setState({
+                    validation_message: "Ready, set, go!"
+                })
+                this.props.set_email(value);
+            } else {
+                this.props.set_email(value);
+                this.setState({
+                    validation_message: "Email is valid"
+                })
+            }
+        },300)
         
     }
 }
