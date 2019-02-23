9c141dc30ce90cd5fb42c9c5d8d2ea9dcbdb36cd
gigi <gianluigilamera@yahoo.com>
2019-02-23 22:13:37 +0000

    Modified percentage tool selector

--- src/components/percentage_tool.jsx
+++ src/components/percentage_tool.jsx
@@ -5,17 +5,17 @@ import ProgressButton from './progress_button';
 const generateMessages = (margin) => {
     switch(margin) {
         case 1: case 2:
-            return "Usually happens in a matter of days";
+            return <p className="hint ok f_100">Usually happens in a matter of days.</p>;
         case 3:
-            return "The best and quickest target is indeed 3%";
+            return <p className="hint ok f_100">The best and quickest target is indeed 3%.</p>;
         case 4: case 5:
-            return "Might require a few weeks";
+            return <p className="hint ok f_100">Might require some time.</p>;
         case 6: case 7:
-            return "It starts to get unlikely.";
+            return <p className="hint alert f_100">It starts getting unlikely.</p>;
         case 8: case 9: case 10:
-            return "Are we talking black monday?";
+            return <p className="hint red_alert f_100">Are we talking black monday?</p>;
         default:
-            return "Click on the + and - buttons to change the target.";
+            return <p className="hint empty f_100">Click on the plus and minus button to change the target.</p>;
     }
 }
 
@@ -24,12 +24,23 @@ export default function PercentageTool(props){
 
     return (
         <form action="/">
-            <div>
-                <div className="minus_handle" onClick={props.decrease_margin}>&minus;</div>
-                <div id="percentage_value">{props.margin}</div>
-                <div className="plus_handle" onClick={props.increase_margin}>&#43;</div>
+            <div className="percentage_container p10 flex_r_wrap align_center justify_center">
+                <div className="minus_handle" onClick={props.decrease_margin}>
+                    <button  type="button" className="btn outline_contrast flew_r_nowrap align_center justify_center">
+                        <img src={require("../images/interface_icons/minus.svg")} alt="Minus" arial-label="subtract" aria-labelledby="subtract"/>
+                    </button>
+                </div>
+                <div className="percentage_value flex_r_nowrap align_center justify_center">
+                    <span className="flex_r_nowrap align_center justify_center">
+                        {props.margin}
+                    </span>
+                </div>
+                <div className="plus_handle" onClick={props.increase_margin}>
+                    <button type="button"className="btn outline_contrast flew_r_nowrap align_center justify_center">
+                    <img src={require("../images/interface_icons/plus.svg")} alt="Minus" arial-label="subtract" aria-labelledby="subtract"/>
+                    </button></div>
+                {generateMessages(props.margin)}
             </div>
-            <p>{generateMessages(props.margin)}</p>
             <ProgressButton >NEXT</ProgressButton>
         </form>
     )
