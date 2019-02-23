9780095da5efb6fb665f7106dec2e1f23169d77b
gigi <gianluigilamera@yahoo.com>
2019-02-23 22:12:03 +0000

    Modified currencies datails

---
+++ src/currencies/currencies_details.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        name: "US Dollar",
+        symbol: "USD"
+    },
+    {
+        name: "Euro",
+        symbol: "EUR"
+    },
+    {
+        name: "Japanese Yen",
+        symbol: "JPY"
+    },
+    {
+        name: "UK Pound",
+        symbol: "GBP"
+    },
+    {
+        name: "Australian Dollar",
+        symbol: "AUD"
+    },
+    {
+        name: "Canadian Dollar",
+        symbol: "CAD"
+    },
+    {
+        name: "Swiss Franc",
+        symbol: "CHF"
+    },
+    {
+        name: "Chinese Yuan",
+        symbol: "CNY"
+    },
+    {
+        name: "Swedish Krona",
+        symbol: "SEK"
+    },
+    {
+        name: "South African Rand",
+        symbol: "ZAR"
+    },
+    {
+        name: "Russian Ruble",
+        symbol: "RUB"
+    },
+]
