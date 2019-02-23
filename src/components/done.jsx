21e2cfa3b4de1f867e2a5d29a10dbbd350692dfd
gigi <gianluigilamera@yahoo.com>
2019-02-23 22:41:43 +0000

    Updated done screen

--- src/components/done.jsx
+++ src/components/done.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ProgressButton from './progress_button';
+import Headings from './headings';
+
+import "./done.scss"
 
 export default function DonePage(props) {
     return (
-        <>
+        <>  
+            <div className="confirm">
             <h3>HOORAY! You're all set!</h3>
-            <p>Now fingers crossed! <span role="img" aria-label="fingers crossed image">🤞</span> <br/>You can also set up another reminder with the button below!</p>
-            <ProgressButton>START AGAIN</ProgressButton>
-            <ProgressButton>DONE</ProgressButton>
+                <p>Now fingers crossed! <span role="img" aria-label="fingers crossed image">🤞</span> <br/>You can also set up another reminder with the button below or you can spread the voice on Facebook. 😊</p>
+                <ProgressButton>START AGAIN</ProgressButton>
+            </div>
         </>
 
     )
