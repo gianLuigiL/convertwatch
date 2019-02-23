5aeb0a06370ec2b135b343dc66d5a2fb84d3ab37
gigi <gianluigilamera@yahoo.com>
2019-02-23 22:43:21 +0000

    Added headings to done

--- src/components/done.jsx
+++ src/components/done.jsx
@@ -7,6 +7,7 @@ import "./done.scss"
 export default function DonePage(props) {
     return (
         <>  
+            <Headings title="Done!" text="Good job!"/>
             <div className="confirm">
             <h3>HOORAY! You're all set!</h3>
                 <p>Now fingers crossed! <span role="img" aria-label="fingers crossed image">🤞</span> <br/>You can also set up another reminder with the button below or you can spread the voice on Facebook. 😊</p>
