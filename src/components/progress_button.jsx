2a8961b41a4cf51646b3410bd5d799dcc19788fc
gigi <gianluigilamera@yahoo.com>
2019-02-23 22:14:28 +0000

    Modified progress button animations

--- src/components/progress_button.jsx
+++ src/components/progress_button.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./progress_button.scss";
 
 export default function ProgressButton(props){
     return (
-        <button onClick={props.clickHandler} type="submit" className="btn btn-lg outline_contrast main_background">{props.children}</button>
+        <button onClick={props.clickHandler} type="submit" className="progress btn btn-lg outline_contrast main_background">
+            <span>
+               {props.children}
+            </span>
+        </button>
     )
 }
 
