a0def049fbab20bea955b06bfcbe5cd7b6524da2
gigi <gianluigilamera@yahoo.com>
2019-02-24 03:06:32 +0000

    Modified header with routing

--- src/components/app_header.jsx
+++ src/components/app_header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import "./app_header.scss";
 
 export default function AppHeader(props) {
     return (
         <header className="flex_r_nowrap align_center justify_start">
-            <div className="branding flex_r_nowrap align-center flex_auto">
-                <span className="image">
-                    <img src="./assets/images/goggles.svg" alt="Logo"/>
-                </span>
-                <span className="app_name">
-                    <span>ConvertWatch</span>
-                </span>
+            <div className="branding">
+                <NavLink to="/" className="flex_r_nowrap align-center flex_auto">
+                    <span className="image">
+                        <img src="./assets/images/goggles.svg" alt="Logo"/>
+                    </span>
+                    <span className="app_name">
+                        <span>ConvertWatch</span>
+                    </span>            
+                </NavLink>
             </div>
             <div className="spacer"></div>
             <div className="flex_r_nowrap align-center">
