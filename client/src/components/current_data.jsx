e50db84730034c542d69230d3e01650fb796e146
gigi <gianluigilamera@yahoo.com>
2019-02-25 16:54:47 +0000

    updated styles for data breakdown

--- client/src/components/current_data.jsx
+++ client/src/components/current_data.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import "./current_data.scss";
 
 export default function CurrentData(props) {
     return (
@@ -17,9 +18,11 @@ export default function CurrentData(props) {
                 <h5>Target Margin</h5>
                 <p>{props.margin_value.toFixed(4)}</p>    
             </div>
-            <h3>What am I doing here?</h3>
-            <p>You want to buy foreign currency when it's weak against the currency you already own so that you can buy more for the same price. Sort of a discount.</p>
-            <p>This means that the more you increase the margin, the more you expect the foreign currency to be weaker against yours, and the higher you go the unlikelier it gets.</p>
+            <div className="breakdown">
+                <h3>What am I looking at?</h3>
+                <p>You want to buy foreign currency when it's weak against the currency you already own so that you can buy more for the same price. Sort of a discount.</p>
+                <p>This means that the more you increase the margin, the more you expect the foreign currency to be weak against yours, and the higher you go the unlikelier it gets.</p>
+            </div>
         </div>
     )
 }
