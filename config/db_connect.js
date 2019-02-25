712488238f98a203ad5051fc4d1f8ae64f430a6e
gigi <gianluigilamera@yahoo.com>
2019-02-25 03:24:16 +0000

    Test database heroku

--- config/db_connect.js
+++ config/db_connect.js
@@ -1,10 +1,10 @@
-let database = "";
+let database = "mongodb://heroku_tld6rz1j:62h46paeuqh4g3hi7h2erlpe0c@ds139435.mlab.com:39435/heroku_tld6rz1j";
 
-if(process.env.PORT) {
+/* if(process.env.PORT) {
     database = "mongodb://heroku_tld6rz1j:62h46paeuqh4g3hi7h2erlpe0c@ds139435.mlab.com:39435/heroku_tld6rz1j";
 } else {
     database = "mongodb://localhost:27017/Convertwatch";
-}
+} */
 
 module.exports = {
     database
